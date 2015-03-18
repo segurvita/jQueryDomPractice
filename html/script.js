a6692d51da68c1e42dc5a9c2df584e28cb76cbd0
segur <segur.opus@gmail.com>
2015-03-18 13:47:00 +0900

    初期

---
+++ html/script.js
@@ -0,0 +1,8 @@
+$(function () {
+    $("img#foo_logo").click(function () {
+        alert("foo fooのロゴをクリックしました。");
+    });
+    $("img#foo_logo").click(function () {
+        alert("fooのロゴをクリックしました。");
+    });
+});
